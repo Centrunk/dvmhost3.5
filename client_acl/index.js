61685925adb41f27826321ca59e8fced9f180888
Nine-JuanJuan <57419652+Nine-JuanJuan@users.noreply.github.com>
2023-07-02 21:27:58 -0500

    Added Config.yml per PHP and updated index

--- client_acl/index.js
+++ client_acl/index.js
@@ -8,22 +8,46 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 import yaml from 'js-yaml';
-
-
-const fneHost = `https://export.acl.centrunk.agency/update/host/acl`;
-const acl_path = "/opt/centrunk/configs/rid_acl.dat";
-const dvm_config_path = "/opt/centrunk/configs/";
-
-var dvm_node_id = null;
-function logInfo(str){
+function logInfo(str, log_path){
+    try {
+        fs.appendFileSync(log_path, "LOG: INFO: " + str);
+    }
+    catch (err){
+        console.log("LOG: ERROR: " + err);
+    }
     console.log("LOG: INFO: " + str);
 }
-function logWarning(str){
+function logWarning(str, log_path){
+    try {
+        fs.appendFileSync(log_path, "LOG: WARNING: " + str);
+    }
+    catch (err){
+        console.log("LOG: ERROR: " + err);
+    }
     console.log("LOG: WARNING: " + str);
 }
-function logError(str){
+function logError(str, log_path){
+    try {
+        fs.appendFileSync(log_path, "LOG: ERROR: " + str);
+    }
+    catch (err){
+        console.log("LOG: ERROR: " + err);
+    }
     console.log("LOG: ERROR: " + str);
 }
+try {
+    const config_file_data = yaml.load(fs.readFileSync("config.yml", 'utf8'));
+    var fneHost = config_file_data.fneHost;
+    var acl_path = config_file_data.aclPath;
+    var dvm_config_path = config_file_data.dvmConfigPath;
+    var log_path = config_file_data.logLocation;
+    logInfo(`Read Config Data:\n   FNE Host: ${fneHost}\n   ACL Write Path: ${acl_path}\n   DVM Path: ${dvm_config_path}\n   client_acl Path: ${log_path}`, log_path)
+} catch (error) {
+    console.log('Error parsing config file: ', error);
+}
+
+var dvm_node_id = null;
+
 const fileNames = ['configCC1.yml', 'configVC1.yml', 'configCONV1.yml', 'configDVRS1.yml'];
 let found = false;
 
@@ -45,7 +69,7 @@ for (const fileName of fileNames) {
 }
 
 if (!found) {
-    logWarning("No DVM Configs Found");
+    logWarning("No DVM Configs Found", log_path);
 }
 if (!dvm_node_id){
     dvm_node_id = "Error with Node ID";
@@ -63,10 +87,10 @@ if (response.ok) {
 
     try {
         fs.writeFileSync(acl_path, rid_acl_data);
-        logInfo("Wrote data to CSV: " + acl_path);
+        logInfo("Wrote data to CSV: " + acl_path, log_path);
     } catch (err) {
-        logError("Error writing to: " + acl_path);
+        logError("Error writing to: " + acl_path, log_path);
     }
 } else {
-    logError('There was an error with ACL! Failed to send command. Contact NET OPS');
+    logError('There was an error with ACL! Failed to send command. Contact NET OPS', log_path);
 }
