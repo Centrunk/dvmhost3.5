1af658d191484cfb1df22dac0579abc7bb3b80cf
Nine-JuanJuan <57419652+Nine-JuanJuan@users.noreply.github.com>
2023-07-16 21:12:55 -0500

    V2 of Script
    Updated script to poll systemd

--- tools/updatedvmhost.sh
+++ tools/updatedvmhost.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+is_service_running() {
+    local service_name=$1
+    systemctl is-active --quiet "$service_name"
+    return $?
+}
+
+restart_service() {
+    local service_name=$1
+    systemctl restart "$service_name"
+}
+
+services=$(ls /etc/systemd/system | grep centrunk)
+
+for service in $services; do
+    if is_service_running "$service"; then
+        systemctl stop "$service"
+        sleep 5
+        while is_service_running "$service"; do
+            sleep 1
+        done
+    fi
+done
+
 rm -rf /opt/centrunk/dvmhost/dvmhost
 rm -rf /opt/centrunk/dvmhost/dvmcmd
 rm -rf /opt/centrunk/utils
@@ -9,7 +32,6 @@ architecture=$(uname -m)
 
 source_path="/opt/centrunk/utils/bin"
 destination_path="/opt/centrunk/dvmhost/"
-
 
 if [[ $architecture == "x86_64" ]]; then
     cp -r "$source_path/x86_64/." "$destination_path"
@@ -43,10 +65,12 @@ elif [[ $architecture == "armv7l" ]]; then
 
 else
     echo "Unsupported architecture: $architecture"
-    echo "Contact Netops for Suppport"
+    echo "Contact Netops for Support"
     exit 1
 fi
 
-echo "Your host is restarting"
-sleep 10
-reboot
+for service in $services; do
+    if ! is_service_running "$service"; then
+        restart_service "$service"
+    fi
+done
